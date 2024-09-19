ceb757ec4af2bc3c5ee33372bae6323385747008
Nguyen Thang <thang.nd0226@gmail.com>
2024-09-19 22:47:04 +0700

    add files

---
+++ dictionary.js
@@ -0,0 +1,55 @@
+let engArr = [
+  "apple",
+  "banana",
+  "grape",
+  "dog",
+  "cat",
+  "pig",
+  "chicken",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten",
+];
+let vietArr = [
+  "táo",
+  "chuối",
+  "nho",
+  "chó",
+  "mèo",
+  "lợn",
+  "gà",
+  "một",
+  "hai",
+  "ba",
+  "bốn",
+  "năm",
+  "sáu",
+  "bảy",
+  "tám",
+  "chín",
+  "mười",
+];
+let viet_word = "";
+
+function trans() {
+  console.log("called");
+  let eng_word = document.getElementById("eng_word").value;
+  eng_word = eng_word.toLowerCase();
+  let index = engArr.indexOf(eng_word);
+
+  if (index > -1) {
+    viet_word = vietArr[index];
+  } else {
+    viet_word =
+      "<span style='color:red'>Không tìm thấy từ trong từ điển</span>";
+  }
+
+  document.getElementById("viet_word").innerHTML = viet_word;
+}
